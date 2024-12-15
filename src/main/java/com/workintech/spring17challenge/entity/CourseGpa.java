c2f3e8d4a41006e65f7832827a41be16b5c3de61
replakcan <alper.makcan@gmail.com>
2024-12-15 18:15:50 +0300

    Creates Course, Grade classes and Gpa Interface

---
+++ src/main/java/com/workintech/spring17challenge/entity/CourseGpa.java
@@ -0,0 +1,6 @@
+package com.workintech.spring17challenge.entity;
+
+public interface CourseGpa {
+
+    int getGpa();
+}
---
+++ src/main/java/com/workintech/spring17challenge/entity/LowCourseGpa.java
@@ -0,0 +1,8 @@
+package com.workintech.spring17challenge.entity;
+
+public class LowCourseGpa implements CourseGpa {
+    @Override
+    public int getGpa() {
+        return 3;
+    }
+}
